0ad0a8c30c89c614ad24552dba8b5fcce4383a71
Warren Reader <WarrenDReader@gmail.com>
2018-03-29 16:38:16 -0600

    Udpate

--- src/components/Ambience.vue
+++ src/components/Ambience.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="container">
     <header><h1>Ambience</h1></header>
+        <!-- <div @click="play()">
+          Hello
+          <audio id="audio" :src="birds">
+          </audio>
+        </div> -->
     <div class="icon-container">
       <div class="icon-column">
-    <img src="../assets/Thunder.svg">
-    <img src="../assets/Rain.svg">
-    <img src="../assets/Ocean.svg">
-    <img src="../assets/Wind.svg">
-    </div>
-    <div class="icon-column">
-    <img src="../assets/Birds.svg">
-    <img src="../assets/Cafe.svg">
-    <img src="../assets/Crickets.svg">
-    <img src="../assets/Fire.svg">
-    <input type="range"/>
-    <audio autoplay src="../assets/sounds/Birds.m4a"/> 
-    </div>
+          <img src="../assets/Thunder.svg" @click="(e) => play(e)" ambience="thunder" status="off">
+          <audio id="thunder" :src="thunder" ></audio>
+
+          <img src="../assets/Birds.svg" @click="(e) => play(e)" ambience="birds" status="off">
+          <audio id="birds" :src="birds"></audio>
+
+          <img src="../assets/Ocean.svg" @click="(e) => play(e)" ambience="ocean" status="off">
+          <audio id="ocean" :src="ocean"></audio>
+
+          <img src="../assets/Wind.svg" @click="(e) => play(e)" ambience="wind" status="off">
+          <audio id="wind" :src="wind"></audio>
+      </div>
+
+      <div class="icon-column">
+        <img src="../assets/Rain.svg" @click="(e) => play(e)" ambience="rain" status="off">
+        <audio id="rain" :src="rain"></audio>
+
+        <img src="../assets/Cafe.svg" @click="(e) => play(e)" ambience="cafe" status="off">
+        <audio id="cafe" :src="cafe"></audio>
+
+        <img src="../assets/Crickets.svg" @click="(e) => play(e)" ambience="crickets" status="off">
+        <audio id="crickets" :src="crickets"></audio>
+
+        <img src="../assets/Fire.svg" @click="(e) => play(e)" ambience="fire" status="off">
+        <audio id="fire" :src="fire"></audio>
+
+      </div>
     </div>
   </div>
 </template>
@@ -24,7 +43,32 @@
 
 export default {
   name: 'Ambience',
+  data() {
+    return {
+      thunder: require('../assets/sounds/Thunder.mp3'),
+      birds: require('../assets/sounds/Birds.mp3'),
+      cafe: require('../assets/sounds/Cafe.mp3'),
+      crickets: require('../assets/sounds/Crickets.mp3'),
+      fire: require('../assets/sounds/Fire.mp3'),
+      ocean: require('../assets/sounds/Ocean.mp3'),
+      rain: require('../assets/sounds/Rain.mp3'),
+      wind: require('../assets/sounds/Wind.mp3'),
+    }
+  },
+  methods: {
+    play(e) {
+      console.log('hit');
+      
+      if(e.target.attributes.status.value === 'off') {
+        e.target.attributes.status.value = 'on';
+        document.getElementById(e.target.attributes.ambience.value).play()
+      } else if (e.target.attributes.status.value === 'on') {
+        e.target.attributes.status.value = 'off';
+        document.getElementById(e.target.attributes.ambience.value).pause()
+      }
 
+    }
+  }
 }
 </script>
 
@@ -46,6 +90,7 @@ img {
   height: 100px;
   width: 100px;
   padding: 20px;
+  cursor:pointer;
 }
 
 </style>
