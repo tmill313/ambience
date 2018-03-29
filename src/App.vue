cb8f861d4459c1fe1db2f70327a01e9ae7b21732
Taylor Miller <tmill313@gmail.com>
2018-03-29 15:08:06 -0600

    initial push

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Ambience msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Ambience from './components/Ambience.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Ambience
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    background-color: #21D19F;
+    font-family: 'Raleway', sans-serif;
+     color: white;
+
 }
 </style>
---
+++ src/components/Ambience.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <header><h1>Ambience</h1></header>
+    <div class="icon-container">
+      <div class="icon-column">
+    <img src="../assets/Thunder.svg">
+    <img src="../assets/Rain.svg">
+    <img src="../assets/Ocean.svg">
+    <img src="../assets/Wind.svg">
+    </div>
+    <div class="icon-column">
+    <img src="../assets/Birds.svg">
+    <img src="../assets/Cafe.svg">
+    <img src="../assets/Crickets.svg">
+    <img src="../assets/Fire.svg">
+    <input type="range"/>
+    <audio autoplay src="../assets/sounds/Birds.m4a"/> 
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Ambience',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container, .icon-column {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.icon-container {
+  display: flex;
+  flex-direction: row;
+}
+.icon-column:last-of-type {
+  margin-left: 55px;
+}
+img {
+  height: 100px;
+  width: 100px;
+  padding: 20px;
+}
+
+</style>
